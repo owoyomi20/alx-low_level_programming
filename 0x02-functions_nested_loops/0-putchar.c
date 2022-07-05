cb22521f5504ea0e62f283a363a14660ab09898d
Taiwo <owoyomitaiwoadeyemi@gmail.com>
2022-07-05 05:43:59 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
-
-
 #include "main.h"
 
 /**
- * main - Prints "putchar" followed by a new line.
- *
+ * main - Prints putchar
  * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-	return (0);
+    return (0);
 }
